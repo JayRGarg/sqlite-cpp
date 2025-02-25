bd2e335caec2aa0741b2a82991737f0afa3c8889
JayRGarg <jgarg20@berkeley.edu>
2025-02-24 17:15:47 -0800

    implement primitive table count in .dbinfo

--- src/Server.cpp
+++ src/Server.cpp
@@ -18,6 +18,8 @@ int main(int argc, char* argv[]) {
     std::string database_file_path = argv[1];
     std::string command = argv[2];
 
+    int HEADER_SIZE = 100;
+
     if (command == ".dbinfo") {
         std::ifstream database_file(database_file_path, std::ios::binary);
         if (!database_file) {
@@ -27,13 +29,19 @@ int main(int argc, char* argv[]) {
 
         // Uncomment this to pass the first stage
         database_file.seekg(16);  // Skip the first 16 bytes of the header
-        
+         
         char buffer[2];
         database_file.read(buffer, 2);
-        
+         
         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        
+
+        database_file.seekg(HEADER_SIZE + 3);
+        database_file.read(buffer, 2);
+
+        unsigned short num_tables = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+         
         std::cout << "database page size: " << page_size << std::endl;
+        std::cout << "number of tables: " << num_tables << std::endl;
     }
 
     return 0;
